70879fd35312c0ac21cd245fef8e0e3e351bc922
vay3t <vay3t@github.com>
2020-01-16 15:16:25 -0300

    update and fix

--- crawl.sh
+++ crawl.sh
@@ -22,26 +22,41 @@ function in_array() {
 
 function obtain_urls(){
 	for link in $(curl -s $1 | pup 'a attr{href}' | sort -u); do
-		echo $link | egrep "https?://" | grep $(domain $1) &> /dev/null
-		if [ $? -eq 0 ]; then
-			new_link=$(echo $link | sed -e "s/https\?\:\/\/$(domain $1)//g")
-			if [ ! -z $new_link ]; then
-				booleano=$(in_array $new_link "${arr[@]}" && echo yes || echo no)
-				if [ $booleano == "no" ]; then
-					arr=("${arr[@]}" $new_link)
-					echo $link
-					obtain_urls $link
-				fi
-			fi
-		else
-			echo $link | egrep "^/" &> /dev/null
+		if [[ $link != "EOF" ]]; then 
+			echo $link | egrep "https?://" | grep $domain_url &> /dev/null
 			if [ $? -eq 0 ]; then
-				new_link="$(protocol $1)://$(domain $1)$link"
-				booleano=$(in_array $link "${arr[@]}" && echo yes || echo no)
-				if [ $booleano == "no" ]; then
-					arr=("${arr[@]}" $new_link)
-					echo $new_link
-					obtain_urls $new_link
+				new_link=$link
+				if [ ! -z $new_link ]; then
+					booleano=$(in_array $new_link "${arr[@]}" && echo yes || echo no)
+					if [ $booleano == "no" ]; then
+						arr=("${arr[@]}" $new_link)
+						echo $link
+						obtain_urls $link
+					fi
+				fi
+			else
+				echo $link | egrep "^//" &> /dev/null
+				if [ $? -eq 0 ]; then
+					if echo $link | grep $domain_url &> /dev/null ; then
+						new_link="$(protocol $1):$link"
+						booleano=$(in_array $new_link "${arr[@]}" && echo yes || echo no)
+						if [ $booleano == "no" ]; then
+							arr=("${arr[@]}" $new_link)
+							echo $new_link
+							obtain_urls $new_link
+						fi
+					fi
+				else
+					echo $link | egrep "^/" &> /dev/null
+					if [ $? -eq 0 ]; then
+						new_link="$(protocol $1)://$(domain $1)$link"
+						booleano=$(in_array $new_link "${arr[@]}" && echo yes || echo no)
+						if [ $booleano == "no" ]; then
+							arr=("${arr[@]}" $new_link)
+							echo $new_link
+							obtain_urls $new_link
+						fi
+					fi
 				fi
 			fi
 		fi
@@ -52,15 +67,16 @@ function obtain_urls(){
 if [ $# -ne 1 ]; then
 	echo "usage: bash $0 http://target.lul"
 else
-	curl -I $1 &> /dev/null
+	echo $1 | egrep "https?://" &> /dev/null
 	if [ $? -eq 0 ]; then
-		echo $1 | egrep "https?://" &> /dev/null
+		domain_url=$(domain $1)
+		curl -I $1 &> /dev/null
 		if [ $? -eq 0 ]; then
 			obtain_urls $1
 		else
-			echo "please add protocol http:// or https://"
+			echo "[-] Problems with connect to $1"
 		fi
 	else
-		echo "[-] Problems with connect to $1"
+		echo "please add protocol http:// or https://"
 	fi
 fi
